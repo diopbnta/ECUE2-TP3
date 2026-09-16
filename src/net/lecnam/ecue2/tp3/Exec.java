47efa0ebe644c72a085aeb97caf97fb66f76dcd1
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-16 16:53:00 +0200

    Exercice 1

--- src/net/lecnam/ecue2/tp3/Exec.java
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -7,6 +7,27 @@ public class Exec  {
 
     public static void main(String[] args) {
 
+
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        Point p = new Point(2, 3);
+        p.translate(3,3);
+        System.out.println(p);
+
+        System.out.println(p1.equals(p2));
+
+        System.out.println("Distance : " + p1.retourneDistance(p2));
+
+        p1.translate(3, 4);
+
+        System.out.println(p1);
+        System.out.println(p1.equals(p2));
+
+
     }
 
 }
---
+++ src/net/lecnam/ecue2/tp3/Point.java
@@ -0,0 +1,52 @@
+package net.lecnam.ecue2.tp3;
+
+
+public class Point {
+
+    // attributs
+    private double x;
+    private double y;
+
+    // constructeur
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // méthode pour déplacer le point
+    public void translate(double x, double y) {
+        this.x += x;
+        this.y += y;
+    }
+
+    // comparer deux points
+    @Override
+    public boolean equals(Object p) {
+        if (this == p) {
+            return true;
+        }
+
+        if (!(p instanceof Point)) {
+            return false;
+        }
+
+        Point point = (Point) p;
+
+        return this.x == point.x && this.y == point.y;
+    }
+
+    // afficher le point
+    @Override
+    public String toString() {
+        return "Point{x=" + x + ", y=" + y + "}";
+    }
+
+    // calculer la distance entre deux points
+    public double retourneDistance(Point p) {
+        double dx = this.x - p.x;
+        double dy = this.y - p.y;
+
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+}
+
