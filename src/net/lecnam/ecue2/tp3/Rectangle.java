12da03698ad917b24c7043163dbd1e9a9d15ef3c
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-21 10:44:29 +0200

    Exercice 2

---
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -0,0 +1,23 @@
+package net.lecnam.ecue2.tp3;
+
+public class Rectangle {
+
+
+
+    // attributs
+    private double longueur;
+    private double largeur;
+    private Point origine;
+    private double coord1;
+    private double coord2;
+
+    // constructeur
+    public Rectangle(double longueur, double largeur, Point origine) {
+        this.longueur = longueur;
+        this.largeur = largeur;
+        this.origine = origine;
+    }
+
+
+
+}
