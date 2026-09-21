72167a2899fa3fd6286538ee2004dbfe3ff682dd
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-21 13:49:01 +0200

    Exercice 7

---
+++ src/net/lecnam/ecue2/tp3/Dessin.java
@@ -0,0 +1,11 @@
+package net.lecnam.ecue2.tp3;
+
+public class Dessin {
+    private Rectangle rectangles;
+    private int nombreRectacles;
+
+    public Dessin(){
+        rectangles = new Rectangle(10);
+        nombreRectacles = 0;
+    }
+}
